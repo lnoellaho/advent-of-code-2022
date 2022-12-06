f7cf305bc8978e3c103c463d3f3df2d3209d39f9
lnoellaho <lnoellaho@gmail.com>
2022-12-06 01:15:41 -0800

    day 5 part 1

--- day_5/part_1.py
+++ day_5/part_1.py
@@ -1,3 +1,4 @@
+from collections import deque
 from util.generate_input_lines import generate_input_lines, generate_list_segments
 
 #             [M] [S] [S]
@@ -20,6 +21,8 @@ from util.generate_input_lines import generate_input_lines, generate_list_segmen
 # ['[R] ', '[H] ', '[N] ', '[P] ', '[J] ', '[Q] ', '[B] ', '[C] ', '[F]']
 # [' 1  ', ' 2  ', ' 3  ', ' 4  ', ' 5  ', ' 6  ', ' 7  ', ' 8  ', ' 9 ']
 # []
+
+
 def main():
     should_create_stacks = True
     crate_stacks = {}
@@ -34,24 +37,43 @@ def main():
             if len(row) == 0:
                 should_create_stacks = False
                 continue
-            
-            for idx, cell in enumerate(row):
-                stack_number = idx+1
-                crate_labels = list(filter(str.isalpha, cell))
-                stack[stack_number].
 
-                    
+            _add_input_row_to_crate_stacks(row, crate_stacks)
+
         else:
-            pass
-        # number_commands = [
-        #     int(word)
-        #     for word in rearrangement_procedure_step.split(" ")
-        #     if word.isdigit()
-        # ]
+            number_commands = [
+                int(word) for word in input_line.split(" ") if word.isdigit()
+            ]
+
+            number_of_crates_to_move = number_commands[0]
+            stack_to_move_from = number_commands[1]
+            stack_to_move_to = number_commands[2]
+
+            for _ in range(0, number_of_crates_to_move):
+                try:
+                    moving_crate = crate_stacks[stack_to_move_from].pop()
+                    crate_stacks[stack_to_move_to].append(moving_crate)
+
+                except IndexError:
+                    pass
+
+    print(crate_stacks)
+
+    crates_on_top = [crate_stacks[idx + 1].pop() for idx in range(0, len(crate_stacks))]
+    print("".join(crates_on_top))
 
 
-def _create_crate_stacks():
-    pass
+def _add_input_row_to_crate_stacks(input_row, crate_stacks):
+    for idx, cell in enumerate(input_row):
+        stack_number = idx + 1
+        crate_label_list = list(filter(str.isalpha, cell))
+        crate_label = crate_label_list[0] if crate_label_list else None
+
+        if crate_label:
+            try:
+                crate_stacks[stack_number].appendleft(crate_label)
+            except KeyError:
+                crate_stacks[stack_number] = deque([crate_label])
 
 
 if __name__ == "__main__":
