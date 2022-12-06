81fccb09be716f9b55d136176019291e7e515477
lnoellaho <lnoellaho@gmail.com>
2022-12-06 10:21:38 -0800

    day 6 part 1 part 2

---
+++ day_6/part_1.py
@@ -0,0 +1,37 @@
+from collections import deque
+from util.generate_input_lines import generate_input_lines
+
+START_OF_PACKET_MARKER_CHARACTER_LIMIT = 4
+
+
+def main():
+    datastream_buffer = list(list(generate_input_lines("day_6/input.txt"))[0])
+
+    first_potential_start_of_packet_marker = datastream_buffer[
+        :START_OF_PACKET_MARKER_CHARACTER_LIMIT
+    ]
+    if (
+        len(set(first_potential_start_of_packet_marker))
+        == START_OF_PACKET_MARKER_CHARACTER_LIMIT
+    ):
+        return START_OF_PACKET_MARKER_CHARACTER_LIMIT
+
+    potential_start_of_packet_marker = deque(first_potential_start_of_packet_marker)
+
+    for idx, signal_character in enumerate(
+        datastream_buffer[START_OF_PACKET_MARKER_CHARACTER_LIMIT:]
+    ):
+        potential_start_of_packet_marker.popleft()
+        potential_start_of_packet_marker.append(signal_character)
+
+        if (
+            len(set(potential_start_of_packet_marker))
+            == START_OF_PACKET_MARKER_CHARACTER_LIMIT
+        ):
+            current_character_count = idx + START_OF_PACKET_MARKER_CHARACTER_LIMIT + 1
+            print(current_character_count)
+            break
+
+
+if __name__ == "__main__":
+    main()
